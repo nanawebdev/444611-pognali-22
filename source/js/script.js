52f77fafcf0eb7fc185257cababe3156ba22cc4e
nanawebdev <nanaprettysinger@gmail.com>
2021-04-13 23:39:03 +0300

    добавляет верстку главной страницы

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,6 @@
-const toggle = document.querySelector('.toggle')
-toggle.addEventListener('click', () => {
+// const toggle = document.querySelector('.toggle')
+// const mainPageMainHeader = document.querySelector('.main-page__main-header')
 
-})
+// toggle.addEventListener('click', () => {
+//   mainPageMainHeader.classList.toggle('main-page__main-header--open')
+// })
