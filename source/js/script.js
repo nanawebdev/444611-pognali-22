9c0b026b9a8307f917dc68c500a2b9b5acfc04c7
nanawebdev <nanaprettysinger@gmail.com>
2021-04-29 17:50:08 +0300

    Добавляет модальное окно Бизнес тарифы

--- source/js/script.js
+++ source/js/script.js
@@ -1,19 +1,19 @@
-const optionHeaders = document.querySelectorAll('.option__header')
+// const optionHeaders = document.querySelectorAll('.option__header')
 
-for (let i=0; i < optionHeaders.length; i++) {
-  const optionHeader = optionHeaders[i]
+// for (let i=0; i < optionHeaders.length; i++) {
+//   const optionHeader = optionHeaders[i]
 
-  optionHeader.addEventListener('click', function() {
-    optionHeader.parentElement.classList.toggle('option--expanded')
-  })
-}
+//   optionHeader.addEventListener('click', function() {
+//     optionHeader.parentElement.classList.toggle('option--expanded')
+//   })
+// }
 
-const openCountries = document.querySelector('.open-countries')
-const countryFilterModal = document.querySelector('.country-filter__modal')
+// const openCountries = document.querySelector('.open-countries')
+// const countryFilterModal = document.querySelector('.country-filter__modal')
 
-openCountries.addEventListener('click', function() {
-  countryFilterModal.classList.toggle('country-filter__modal--open')
-})
+// openCountries.addEventListener('click', function() {
+//   countryFilterModal.classList.toggle('country-filter__modal--open')
+// })
 
 // const header = document.querySelector('.main-header')
 
@@ -22,8 +22,23 @@ openCountries.addEventListener('click', function() {
 //     header.classList.add('main-header--scroll')
 // })
 
-const closeCountries = document.querySelector('.close-countries')
+// const closeCountries = document.querySelector('.close-countries')
 
-closeCountries.addEventListener('click', function() {
-  countryFilterModal.classList.remove('country-filter__modal--open')
+// closeCountries.addEventListener('click', function() {
+//   countryFilterModal.classList.remove('country-filter__modal--open')
+// })
+
+// Бизнес тарифы
+
+const showBusinessRates = document.querySelector('.js-show-business-rates')
+const rateModal = document.querySelector('.rate__modal')
+const closeBusinessRates = document.querySelector('.js-close-business-rates')
+
+
+showBusinessRates.addEventListener('click', function() {
+  rateModal.classList.add('rate__modal--open')
+})
+
+closeBusinessRates.addEventListener('click', function() {
+  rateModal.classList.remove('rate__modal--open')
 })
